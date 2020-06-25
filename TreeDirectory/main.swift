5199ca1b723e60eb96ad110a23ac0c1ed3a50429
Cayo M Amorim <cayomesquita@gmail.com>
2020-06-25 11:16:56 -0700

    Initial commit

---
+++ TreeDirectory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TreeDirectory
+//
+//  Created by Cornerstone on 2020-06-25.
+//  Copyright © 2020 Cornerstone. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
