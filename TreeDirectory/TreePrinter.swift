a93282c8f220d84d5994d038f0921b8c8ad8d2a8
Martin Kuchar <kucharmartin0@gmail.com>
2020-06-25 16:51:04 -0700

    Add the feature to print the dir tree without signs

--- TreeDirectory/TreePrinter.swift
+++ TreeDirectory/TreePrinter.swift
@@ -8,16 +8,40 @@
 
 import Foundation
 
-func printDirectioryTree(_ URL: URL) {
-    if !FileManager.default.fileExists(atPath: URL.path) {
+
+func printDirectioryTree(_ urlString: String, _ padding: String = " ") {
+    guard let fileUrl = URL(string: urlString), FileManager.default.fileExists(atPath: fileUrl.path) else {
         return
     }
-    
-    let localFileManager = FileManager()
-    let enumeratedDir = localFileManager.enumerator(atPath: URL.path)
-    
-    let contents = try! FileManager.default.contentsOfDirectory(at: URL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-    for content in contents {
-        print(content.description)
-    }
+    let localFileManager = FileManager.default
+        /// Enumerating the directory when we want to use recursion
+        //        guard let enumeratedDir = localFileManager.enumerator(atPath: URL.path)  else {
+        //            return
+        //        }
+        //     for content in enumeratedDir {
+        //        if let file = content as? String, !file.hasPrefix(".git") {
+        //            let attributes = try! FileManager.default.attributesOfItem(atPath: file)
+        //            print(file)
+        //        }
+        //    }
+
+        var isDirectory : ObjCBool = false
+        if FileManager.default.fileExists(atPath: fileUrl.path, isDirectory:&isDirectory) {
+            if isDirectory.boolValue {
+            // Content is a directory
+            let contents = try! localFileManager.contentsOfDirectory(at: fileUrl, includingPropertiesForKeys: nil,
+                                                                           options: [.skipsHiddenFiles])
+                
+                for content in contents {
+                        let name = FileManager.default.displayName(atPath: content.path)
+//                      print(FileManager.default.displayName(atPath: content.path))
+                        print(padding + name)
+                        printDirectioryTree(urlString + "/" + name, padding + "  ")
+                    }
+            }
+        } else {
+            print(urlString)
+        }
+//    print("Error while enumerating URL path: \(fileUrl.path)")
+
 }
