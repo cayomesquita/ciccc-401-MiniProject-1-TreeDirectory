3391f086c5a95cf484b148fcf70a6efdaf6e42e5
Martin Kuchar <kucharmartin0@gmail.com>
2020-06-26 16:51:51 -0700

    Improve the logic

--- TreeDirectory/TreePrinter.swift
+++ TreeDirectory/TreePrinter.swift
@@ -7,22 +7,13 @@
 //
 
 import Foundation
+import AppKit
 
 func printDirectoryTreeHelper(_ urlString: String, _ padding: String = " ", _ directoryCounter: inout Int, _ fileCounter: inout Int) {
     guard let fileUrl = URL(string: urlString), FileManager.default.fileExists(atPath: fileUrl.path) else {
         return
     }
     let localFileManager = FileManager.default
-        /// Enumerating the directory when we want to use recursion
-        //        guard let enumeratedDir = localFileManager.enumerator(atPath: URL.path)  else {
-        //            return
-        //        }
-        //     for content in enumeratedDir {
-        //        if let file = content as? String, !file.hasPrefix(".git") {
-        //            let attributes = try! FileManager.default.attributesOfItem(atPath: file)
-        //            print(file)
-        //        }
-        //    }
     var isDirectory : ObjCBool = false
     if FileManager.default.fileExists(atPath: fileUrl.path, isDirectory:&isDirectory) {
         if isDirectory.boolValue {
@@ -44,9 +35,10 @@ func printDirectoryTreeHelper(_ urlString: String, _ padding: String = " ", _ di
         }
     }
 }
+
 func printDirectoryTree(_ urlString: String) {
-    var directoryCounter = 0
+    var directoryCounter = -1
     var fileCounter = 0
     printDirectoryTreeHelper(urlString, "", &directoryCounter, &fileCounter)
-    print("\nNumber of directories: \(directoryCounter) | Number of files: \(fileCounter)\n")
+    print("\n\(directoryCounter) directories, \(fileCounter) files\n")
 }
