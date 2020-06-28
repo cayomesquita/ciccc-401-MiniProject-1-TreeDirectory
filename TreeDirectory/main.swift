f62ac5f1b9b57a4d598be8e05dafc8db7ee4461d
Cayo M Amorim <cayomesquita@gmail.com>
2020-06-27 22:33:25 -0700

    Update printing root directory and refactor

--- TreeDirectory/main.swift
+++ TreeDirectory/main.swift
@@ -11,27 +11,31 @@ import AppKit
 
 printDirectoryTree(FileManager.default.currentDirectoryPath)
 
-func printDirectoryTreeHelper(_ urlString: String, _ padding: String = " ", _ directoryCounter: inout Int, _ fileCounter: inout Int) {
+func printDirectoryTreeHelper(_ urlString: String, _ padding: String = " ", _ leading: String = "", _ directoryCounter: inout Int, _ fileCounter: inout Int) {
     let localFileManager = FileManager.default
     var isDirectory : ObjCBool = false
     guard let fileUrl = URL(string: urlString), localFileManager.fileExists(atPath: fileUrl.path, isDirectory: &isDirectory) else {
         return
     }
     
+    let fileName = fileUrl.lastPathComponent
+    print(padding + leading + fileName)
+    
     if isDirectory.boolValue {
         directoryCounter += 1
         let contents = try! localFileManager.contentsOfDirectory(at: fileUrl, includingPropertiesForKeys: nil,
                                      options: [.skipsHiddenFiles])
-            for i in 0...contents.count-1 {
-                let name = localFileManager.displayName(atPath: contents[i].path)
-                if i == contents.count - 1 {
-                    print(padding + "└─" + name)
-                    printDirectoryTreeHelper(urlString + "/" + name, padding + "   ", &directoryCounter, &fileCounter)
-                } else {
-                    print(padding + "├─" + name)
-                    printDirectoryTreeHelper(urlString + "/" + name, padding + "│  ",  &directoryCounter, &fileCounter)
-                }
+        let newPadding = padding + ((leading == "├─") ? "│  " : "   ") // check if the parent is last child
+        
+        for i in 0...contents.count-1 {
+            let name = localFileManager.displayName(atPath: contents[i].path)
+            if i == contents.count - 1 {
+                printDirectoryTreeHelper(urlString + "/" + name, newPadding, "└─", &directoryCounter, &fileCounter)
+            } else {
+                printDirectoryTreeHelper(urlString + "/" + name, newPadding, "├─", &directoryCounter, &fileCounter)
             }
+        }
+        
     } else {
         fileCounter += 1
     }
@@ -41,7 +45,7 @@ func printDirectoryTreeHelper(_ urlString: String, _ padding: String = " ", _ di
 func printDirectoryTree(_ urlString: String) {
     var directoryCounter = -1
     var fileCounter = 0
-    printDirectoryTreeHelper(urlString, "", &directoryCounter, &fileCounter)
+    printDirectoryTreeHelper(urlString, "", "", &directoryCounter, &fileCounter)
     print("\n\(directoryCounter) directories, \(fileCounter) files\n")
 }
 
